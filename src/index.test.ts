d7145e5efcb4d89b3452cf6cd37ee407f5de558d
Hyun Kim <justinkimkorea@gmail.com>
2021-05-31 11:27:45 +1200

    feat: add support for generic types for defining secrets

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,33 +4,47 @@ import rimraf from "rimraf";
 import { expect } from "chai";
 import { getSecretFactory, getSecrets } from "./index";
 
-describe("Test module", function() {
-  it("should return secret values", function(done) {
-    fs.mkdtemp(path.join(__dirname, "SECRET"), (error, folder) => {
+type Secrets = {
+  USERNAME: string;
+  PASSWORD: string;
+};
+
+const username = "userXYZ",
+  password = "DaA3f/Ts21fe!wq?/1";
+
+describe("Test module", function () {
+  it("should return secret values", function (done) {
+    fs.mkdtemp(path.join(__dirname, "SECRETS"), (error, folder) => {
       if (error) {
         rimraf(folder, (error: any) => done(error));
       }
 
-      fs.writeFile(
-        path.join(folder, "SUPSERSECRET"),
-        "secret-value",
-        (error: any) => {
-          if (error) {
-            rimraf(folder, (error: any) => done(error));
-          }
-
-          const secrets = getSecrets(folder);
-          expect(secrets)
-            .to.haveOwnProperty("SUPSERSECRET")
-            .that.equals("secret-value");
-
-          rimraf(folder, (error: any) => done(error));
+      // Create secret files with values.
+      Promise.all([
+        writeFile(path.join(folder, "USERNAME"), username),
+        writeFile(path.join(folder, "PASSWORD"), password),
+      ])
+        .then(() => {
+          const secrets = getSecrets<Secrets>(folder);
+          expect(secrets).to.haveOwnProperty("USERNAME").that.equals(username);
+          expect(secrets).to.haveOwnProperty("PASSWORD").that.equals(password);
 
           const getSecret = getSecretFactory(secrets);
-          const supersecret = getSecret("SUPSERSECRET");
-          expect(supersecret).to.equal("secret-value");
-        }
-      );
+          expect(getSecret("USERNAME")).to.equal(username);
+          expect(getSecret("PASSWORD")).to.equal(password);
+        })
+        .finally(() => {
+          rimraf(folder, (error: any) => done(error));
+        });
     });
   });
 });
+
+function writeFile(path: string, data: any): Promise<void> {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, data, (err) => {
+      if (err) return reject(err);
+      return resolve();
+    });
+  });
+}
--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,9 @@ export interface Secrets {
   [key: string]: string;
 }
 
-export type GetSecretFn = (name: string) => string;
+export type GetSecretFn<S extends Secrets = Secrets> = (name: keyof S) => S[keyof S];
 
-export function getSecrets(secretDir: string): Secrets {
+export function getSecrets<T extends Secrets = Secrets>(secretDir: string): T {
   const secrets: Secrets = {};
   if (fs.existsSync(secretDir)) {
     const files = fs.readdirSync(secretDir);
@@ -23,13 +23,14 @@ export function getSecrets(secretDir: string): Secrets {
       secrets[key] = data;
     });
   }
-  return secrets;
+  return secrets as T;
 }
 
-export function getSecretFactory(secrets: Secrets): GetSecretFn {
-  return (name: string) => secrets[name];
+export function getSecretFactory<S extends Secrets = Secrets>(secrets: S): GetSecretFn<S> {
+  return (name: keyof S) => secrets[name];
 }
 
+// Provide defaults.
 export const SECRET_DIR = "/run/secrets";
-export const secrets: Secrets = getSecrets(SECRET_DIR);
+export const secrets = getSecrets(SECRET_DIR);
 export const getSecret: GetSecretFn = getSecretFactory(secrets);
