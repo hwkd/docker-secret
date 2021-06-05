ed41db67a71144262bedcd885abe7d8976dcf3e9
Hyun Kim <justinkimkorea@gmail.com>
2021-06-05 21:32:21 +1200

    chore: auto build on release

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const SECRET_DIR = "/run/secrets";
-export interface Secrets {
-    [key: string]: string;
-}
-export declare type GetSecretFn<S extends Secrets = Secrets> = (name: keyof S) => S[keyof S];
-export declare function getSecrets<T extends Secrets = Secrets>(secretDir?: string): T;
-export declare function getSecretFactory<S extends Secrets = Secrets>(secrets: S): GetSecretFn<S>;
-export declare const secrets: Secrets;
-export declare const getSecret: GetSecretFn;
